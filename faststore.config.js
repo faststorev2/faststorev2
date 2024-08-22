ce0684d06a55db62a0174d4b25660355b7064a50
Everton Vargas <everton.vargas@vtex.com>
2024-08-22 15:25:03 -0300

    change config

--- faststore.config.js
+++ faststore.config.js
@@ -1,80 +1,99 @@
+
 module.exports = {
   seo: {
-    title: "NextJSStore",
-    description: "Fast Demo Store",
-    titleTemplate: "%s | FastStore",
-    author: "Store Framework",
+    "title": "FastStore",
+    "description": "A fast and performant store framework",
+    "titleTemplate": "%s | FastStore",
+    "author": "FastStore"
   },
-  theme: "custom-theme",
-  platform: "vtex",
+
+  // Theming
+  theme: 'custom-theme',
+
+  // Ecommerce Platform
+  platform: 'vtex',
+
+  // Platform specific configs for API
   api: {
-    storeId: "storeframework",
-    workspace: "master",
-    environment: "vtexcommercestable",
-    hideUnavailableItems: false,
+    storeId: "faststorev2",
+    workspace: 'master',
+    environment: 'vtexcommercestable',
+    hideUnavailableItems: true,
     incrementAddress: false,
   },
+
+  // Default session
   session: {
     currency: {
-      code: "USD",
-      symbol: "$",
+      code: "BRL",
+      symbol: "R$",
     },
-    locale: "en-US",
-    channel: '{"salesChannel":"1","regionId":""}',
-    country: "USA",
+    locale: "pt-BR",
+    channel: '{"salesChannel":1,"regionId":""}',
+    country: "BRA",
     deliveryMode: null,
     addressType: null,
     postalCode: null,
     geoCoordinates: null,
     person: null,
   },
+
   cart: {
-    id: "",
+    id: '',
     items: [],
     messages: [],
     shouldSplitItem: true,
   },
-  storeUrl: "https://vtexfaststore.com",
-  secureSubdomain: "https://secure.vtexfaststore.com",
+
+  // Production URLs
+  storeUrl: "https://faststorev2.vtex.app",
+  secureSubdomain: "https://secure.vtexfaststore.com/",
   checkoutUrl: "https://secure.vtexfaststore.com/checkout",
   loginUrl: "https://secure.vtexfaststore.com/api/io/login",
   accountUrl: "https://secure.vtexfaststore.com/api/io/account",
+
   previewRedirects: {
-    home: "/",
-    plp: "/office",
-    search: "/s?q=headphone",
-    pdp: "/apple-magic-mouse/p",
+    home: '/',
+    plp: "/bebidas",
+    search: "/s?q=Coca%20Cola",
+    pdp: "/produto-willy/p",
   },
+
+  // Lighthouse CI
   lighthouse: {
-    server: "http://localhost:3000",
+    server: process.env.BASE_SITE_URL || 'http://localhost:3000',
     pages: {
-      home: "/",
-      pdp: "/apple-magic-mouse/p",
-      collection: "/office",
+      home: '/',
+      pdp: "/produto-willy/p",
+      collection: "/bebidas",
     },
   },
+
+  // E2E CI
   cypress: {
     pages: {
-      home: "/",
-      pdp: "/apple-magic-mouse/p",
-      collection: "/office",
-      collection_2: "/technology",
-      collection_filtered:
-        "/office/?category-1=office&marca=acer&facets=category-1%2Cmarca",
-      search: "/s?q=orange",
+      home: '/',
+      pdp: "/produto-willy/p",
+      collection: "/bebidas",
+      collection_filtered: "/bebidas/?category-1=bebidas&brand=Coca%20Cola&facets=category-1%2Cbrand%27",
+      search: "/s?q=Coca%20Cola",
     },
+    browser: 'electron',
   },
+
   analytics: {
-    gtmContainerId: "GTM-PGHZ95N",
+    // https://developers.google.com/tag-platform/tag-manager/web#standard_web_page_installation,
+    gtmContainerId: "GTM-5KSSR9NK",
   },
+
   experimental: {
     nodeVersion: 18,
     cypressVersion: 12,
   },
-  account: "storeframework",
+
   vtexHeadlessCms: {
     webhookUrls: [
-      "https://storeframework.myvtex.com/cms-releases/webhook-releases",
+      "https://faststorev2.myvtex.com/cms-releases/webhook-releases",
     ],
   },
-};
+}
